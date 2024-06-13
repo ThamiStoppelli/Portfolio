48349f0455522d0f502c298a4f87d989a48e8a4c
ThamiStoppelli <thamires_sr@hotmail.com>
2024-06-13 14:33:28 -0300

    first committing: structuring the components

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Header from './components/Header';
+import Projects from './components/Projects';
+import AboutMe from './components/AboutMe';
+import Resume from './components/Resume';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <AboutMe />
+      <Resume />
+      <Projects />
+    </>
+  )
+}
+
+export default App
