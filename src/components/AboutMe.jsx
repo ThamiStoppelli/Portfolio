dfbffc84fe533e25357c441b91abf13939cf7991
ThamiStoppelli <thamires_sr@hotmail.com>
2024-06-18 18:04:01 -0300

    Created contact and  resume sections

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -3,6 +3,8 @@ import { useLanguage } from '../context/LanguageContext';
 
 // import image from '../assets/thami2.png'
 import image from '../assets/teste5.png'
+import locationIcon from '../assets/location.svg'
+import mailIcon from '../assets/mail.svg'
 import './AboutMe.css'
 
 const translations = {
@@ -10,29 +12,44 @@ const translations = {
     section: "Sobre mim",
     greeting: "Oi, eu sou a Thamires!",
     description: "Atuando como desenvolvedora front-end e UI/UX designer, possuo experiência com desenvolvimento de softwares desde o design das interfaces até o desenvolvimento front-end e back-end da aplicação com modelagem do banco de dados, destacando habilidades com ReactJS, TDD, consumo de APIs e responsividade.",
+    location: "Fortaleza/CE, Brasil",
+    contact: "Contato",
   },
   en: {
     section: "About me",
     greeting: "Hi, I'm Thamires!",
     description: "As a versatile professional with expertise in both front-end development and UI/UX design, I have hands-on experience throughout the entire software development lifecycle, from user interface design to database modeling and web development. My skills include proficiency in ReactJS, TDD, APIs, responsive design, UX research, UI/UX design and usability testing.",
+    location: "Fortaleza/CE, Brazil",
+    contact: "Contact",
   },
 };
 
 function AboutMe() {
 
   const { language } = useLanguage();
-  const { section, greeting, description } = translations[language];
+  const { section, greeting, description, location, contact } = translations[language];
 
   return (
     <div className='aboutme-container' id="about">
       <div className='aboutme'>
-        <h3>{section}</h3>
+        {/* <h3>{section}</h3> */}
         <h4>{greeting}</h4>
         <p>{description}</p>
         {/* baixar pdf do resume */}
       </div>
-      <div>
-        <img src={image} alt='imagem'></img>
+      <div className='image-card'>
+        <img src={image} alt='imagem' className='me'></img>
+        <div className='info-card'>
+          <p id='contact'>{contact}</p>
+          <div className='each'>
+            <img src={locationIcon}/>
+            <p>{location}</p>
+          </div>
+          <div className='each'>
+            <img src={mailIcon}/>
+            <p>thamiresstoppelli@gmail.com</p>
+          </div>
+        </div>
       </div>
     </div>
   )
